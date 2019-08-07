487402f41886004d9f6d160052350e2868e77623
Uzair Akbar <uzair.akbar@tum.de>
2019-08-07 11:04:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='bpy2.79',
+      version='0.1',
+      description='Pre-built Blender 2.79 as a python3.5 module bpy for linux.',
+      author='Uzair Akbar',
+      author_email='uzairakbar@outlook.com',
+      url='https://github.com/uzairakbar/bpy',
+      packages=['bpy'],
+      include_package_data=True
+      )
