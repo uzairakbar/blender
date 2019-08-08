3a3e0004ac6783c4e4b89d939e4432de99026a85
Uzair Akbar <uzair.akbar@tum.de>
2019-08-08 19:35:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup
-from os import path
 from io import open
 
-here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='blender',
-      version='1.3',
+      version='1.4',
       description='Pre-built Blender 2.79 as a python3.5 module bpy for linux.',
       author='Uzair Akbar',
       author_email='uzairakbar@outlook.com',
-      url='https://github.com/uzairakbar/blender/archive/v1.3.tar.gz',
+      url='https://github.com/uzairakbar/blender/archive/v1.4.tar.gz',
       packages=['blender'],
       long_description=long_description,
       long_description_content_type="text/markdown",
