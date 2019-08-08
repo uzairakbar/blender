729cae8db024d404371d3353b8b89fe350f441a3
Uzair Akbar <uzair.akbar@bmw.de>
2019-08-08 14:35:39 +0200

    working version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='blender',
-      version='0.2',
+      version='1.0',
       description='Pre-built Blender 2.79 as a python3.5 module bpy for linux.',
       author='Uzair Akbar',
       author_email='uzairakbar@outlook.com',
-      url='https://github.com/uzairakbar/blender/archive/v0.2.tar.gz',
+      url='https://github.com/uzairakbar/blender/archive/v1.0.tar.gz',
       packages=['blender'],
       include_package_data=True
       )
