c5e7290bf44fa3aceaef65c5ca5919561c8d5f03
Uzair Akbar <uzair.akbar@tum.de>
2019-08-08 16:22:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-
+import pathlib
 from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE/"README.md").read_text()
 
 setup(name='blender',
       version='1.0',
@@ -9,5 +14,7 @@ setup(name='blender',
       author_email='uzairakbar@outlook.com',
       url='https://github.com/uzairakbar/blender/archive/v1.0.tar.gz',
       packages=['blender'],
+      long_description=README,
+      long_description_content_type="text/markdown",
       include_package_data=True
       )
