4f6df0d251712cf944cf7775abc795ed50629e0e
Uzair Akbar <uzair.akbar@tum.de>
2019-08-08 16:42:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE/"README.md").read_text()
 
 setup(name='blender',
-      version='1.0',
+      version='1.1',
       description='Pre-built Blender 2.79 as a python3.5 module bpy for linux.',
       author='Uzair Akbar',
       author_email='uzairakbar@outlook.com',
-      url='https://github.com/uzairakbar/blender/archive/v1.0.tar.gz',
+      url='https://github.com/uzairakbar/blender/archive/v1.1.tar.gz',
       packages=['blender'],
       long_description=README,
       long_description_content_type="text/markdown",
